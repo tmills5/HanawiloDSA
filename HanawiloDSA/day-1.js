893abf90a7cad4500e4e732a722437a4736f6d56
Merle Tyson Mills <tysonmills5@gmail.com>
2023-01-31 12:18:26 -0600

    2,7,8,13 left

--- HanawiloDSA/day-1.js
+++ HanawiloDSA/day-1.js
@@ -37,33 +37,33 @@ const filterArr = (arr) => {
 //todo: Problem 5: 
 
 const someFunction = (arr1) => {
-  arr1.push(1).pop() // O(2)
+  arr1.push(1).pop() //! O(2)
 
   for (let i = 0; i < arr1.length; i++) {
   console.log('do something 2')
-  } // O(n)
+  } //! O(n)
 
   for (let i = 0; i < arr1.length; i++) {
   console.log('do something 3')
-  } // O(n)
+  } //! O(n)
 
   for (let i = 0; i < arr1.length; i++) {
     for (let i = 0; i < arr1.length; i++) {
       console.log('do something 3')
     }
-  } // O(n^2)
+  } //! O(n^2)
 }
 
-// O(2) + O(n) + O(n) + O(n^2) = O(2) + 2O(n) + O(n^2) = O(n^2)
+//! O(2) + O(n) + O(n) + O(n^2) = O(2) + 2O(n) + O(n^2) = O(n^2)
 
 //todo: Problem 6: 
 
 const someFunction1 = (arr1) => {
-  let sum = arr1[1] + arr[2] // O(1) mathematical equation so that is constant time
+  let sum = arr1[1] + arr[2] //! O(1) mathematical equation so that is constant time
 
   while (condition) {
     sum = arr[5] + arr[7]
-  } // O(1) ? O(n) it is a math equation but in a while loop
+  } //! O(1) ? O(n) it is a math equation but in a while loop
 
   for (let i = 0; i < arr1.length; i++) {
     for (let i = 0; i < arr1.length; i++) {
@@ -71,10 +71,9 @@ const someFunction1 = (arr1) => {
         console.log('do something 3')
       }
     }
-  } // O(n^3) 
+  } //! O(n^3) 
 }
 
-// 
 
 //todo: Problem 7: Please explain in 3-5 sentences why we can ignore constants and consolidate our time complexities.
 
@@ -82,10 +81,80 @@ const someFunction1 = (arr1) => {
 
 //todo: Problem 9:  Given the following data TYPES, label what the space complexity is for each one:
 
+/* 
+  Boolean-Constant
+  Undefined-Constant
+  Null-Constant
+  Number-Constant
+  String-Linear
+  Array-Linear
+  Object-Linear
+*/
+
 //todo: Problem 10: Give two reasons when you should use a array and when you should use a object.
+
+/* 
+  Array: When you need fast access and ordering
+  Object: When order doesn't matter and there is no need for fast access
+*/
 
 //todo: Problem 11: Given the following object methods, label what the TIME complexity is for each one:
 
-//todo: Given the following array methods, label what the TIME complexity is for each one:
+const obj = {
+  name: 'tony'
+  }
+
+  //inserting
+  obj.age = 44; //! O(1)
+  //removing
+  delete obj.age; //! O(1)
+  //searching 1
+  obj.hasOwnProperty['name']  //! O(1) because returns true or false if present
+  //searching 2
+  for (const prop in obj) { //! O(n) searching through keys
+  console.log(obj[prop])
+  }
+  //accessing
+  obj.age //44 //! O(1)
+  //retrieving keys
+  Object.keys(obj) //! O(n) because it is returning the keys
+  //retrieving values
+  Object.values(obj) //! O(n) because it is returning the values
+
+//todo: Problem 12: Given the following array methods, label what the TIME complexity is for each one:
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+//inserting 1
+arr2.push(8) //! O(1)
+//inserting 2
+arr2.unshift(0) //! O(n)
+//removing 1
+arr2.pop() //! O(1)
+//removing 2
+arr2.shift() //! O(n)
+//searching 1
+const findNumber = arr2.find(num => num === 2) //! O(n)
+//searching 2
+for (let i = 0; i < arr2.length; i++) {
+if (arr2[i] === 2) {
+return arr2[i]
+}
+} //! O(n) still has to loop through the array
+//retrieving
+const getNumber = arr2[3] //! O(1)
+//method 1
+const double = arr2.map(num => num * 2) //! O(n)
+//method 2
+const removeAndAddNewNumber = arr2.splice(1, 1, 5) //! O(n) changing the indices
+//method 3
+const getSum = arr2.reduce((total, num) => total + num, 0) //! O(n) has to loop to get the values to calculate the sum
+//method 4
+for (const num of nums) {
+console.log(num * 2)
+} //! O(n)  still a loop
+//method 5
+const convertToString = arr2.join(' ') //! O(n)
+
 
 //todo: Problem 13: For each one of these code blocks, please identify the time & space complexity and explanation of why
